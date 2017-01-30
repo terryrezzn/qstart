33ae7931364403972a6cc3adb6da2810cd2183a2
Terry Herrington <terryherrington@Rev-T-Bones-Mac-Pro.local>
2017-01-29 20:23:54 -0500

    added some new verbiage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Welcome {{name}}</h1>`,
+  template: `<h1>Terry says welcome {{name}}</h1>`,
 })
 export class AppComponent  { name = 'Angular'; }
